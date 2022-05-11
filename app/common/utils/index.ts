0843359bf5b103a072ed69b654d7d2c3c41e20bc
Lucas Cairns <lucas.cairns@gmail.com>
2022-05-11 12:03:44 +0100

    Tidy up form bootstrap code

--- app/common/utils/index.ts
+++ app/common/utils/index.ts
@@ -1,34 +1,68 @@
 import FormWizard from 'hmpo-form-wizard'
 import type { Steps, Fields } from 'hmpo-form-wizard'
-import Express from 'express'
-import type { Router } from 'express'
+import type { Router, Request, Response } from 'express'
 
 export interface FormOptions {
   journeyName: string
   journeyTitle: string
   version: number
   entryPoint?: boolean
+  active?: boolean
 }
 
 export interface FormRouterConfig {
   version: number
+  active: boolean
   router: Router
 }
 
-const SetupForm = (steps: Steps, fields: Fields, options: FormOptions): FormRouterConfig => {
-  const router = Express.Router()
+export const setupForm = (steps: Steps, fields: Fields, options: FormOptions, router: Router): FormRouterConfig => {
+  if (options.active === true) {
+    router.get('/fields', (req, res) => res.json(fields))
+    router.use(
+      FormWizard(steps, fields, {
+        journeyName: `${options.journeyName}:${options.version}`,
+        journeyPageTitle: options.journeyTitle,
+        name: `${options.journeyName}:${options.version}`,
+        entryPoint: options.entryPoint || false,
+      })
+    )
+  }
 
-  router.get('/fields', (req, res) => res.json(fields))
-  router.use(
-    FormWizard(steps, fields, {
-      journeyName: `${options.journeyName}:${options.version}`,
-      journeyPageTitle: options.journeyTitle,
-      name: `${options.journeyName}:${options.version}`,
-      entryPoint: options.entryPoint || false,
-    })
-  )
-
-  return { version: options.version, router }
+  return { version: options.version, active: options.active || false, router }
 }
 
-export default { SetupForm }
+interface FormVersionInformation {
+  version: number
+  active: boolean
+}
+
+interface FormVersionResponse {
+  latest: number
+  available: FormVersionInformation[]
+}
+
+const mountRouter = (r: Router) => (form: FormRouterConfig) => r.use(`/v${form.version}`, form.router)
+const getLatestVersionFrom = (formRouterConfig: FormRouterConfig[]): FormRouterConfig | null =>
+  formRouterConfig.reduce(
+    (latest, current) => (!latest || (current.active && current?.version > latest?.version) ? current : latest),
+    null
+  )
+
+export const bootstrapFormConfiguration = (formRouterConfig: FormRouterConfig[], router: Router) => {
+  const latestForm: FormRouterConfig = getLatestVersionFrom(formRouterConfig)
+  const formVersionResponse: FormVersionResponse = {
+    latest: latestForm?.version || null,
+    available: formRouterConfig.map(form => ({ version: form.version, active: form.active })),
+  }
+
+  formRouterConfig.filter((form: FormRouterConfig) => form.active).forEach(mountRouter(router))
+
+  if (latestForm) {
+    router.use('/', latestForm.router)
+  }
+
+  router.use('/versions', (req: Request, res: Response) => res.json(formVersionResponse))
+}
+
+export default { setupForm, bootstrapFormConfiguration }
--- app/poc-form/index.ts
+++ app/poc-form/index.ts
@@ -1,28 +1,10 @@
 import { Router } from 'express'
-import type { Request, Response } from 'express'
-import { FormRouterConfig } from '../common/utils'
-import pocFormRouterV1 from './v1/index'
+import { bootstrapFormConfiguration } from '../common/utils'
+
+import pocFormV1 from './v1/index'
 
 const router = Router()
 
-interface FormVersionInformation {
-  latest: number
-  available: number[]
-}
-
-const mountRouter = (r: Router) => (form: FormRouterConfig) => r.use(`/v${form.version}`, form.router)
-const getLatestVersionFrom = (formRouterConfig: FormRouterConfig[]) =>
-  formRouterConfig.reduce((latest, current) => (current.version > latest.version ? current : latest))
-
-const formRouterConfig: FormRouterConfig[] = [pocFormRouterV1]
-const latestForm: FormRouterConfig = getLatestVersionFrom(formRouterConfig)
-const formVersionInformation: FormVersionInformation = {
-  latest: latestForm.version,
-  available: formRouterConfig.map(form => form.version),
-}
-
-formRouterConfig.forEach(mountRouter(router))
-router.use('/', latestForm.router)
-router.use('/versions', (req: Request, res: Response) => res.json(formVersionInformation))
+bootstrapFormConfiguration([pocFormV1], router)
 
 export default router
